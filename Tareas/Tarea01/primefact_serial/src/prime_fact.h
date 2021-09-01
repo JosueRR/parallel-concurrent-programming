e6707dcece2bd8b6d2f43d958bc3b867df3ac5bb
JosueRR <josue-retana@hotmail.com>
2021-09-01 17:39:31 -0600

    Agrega src definicion subrutinas

---
+++ Tareas/Tarea01/primefact_serial/src/prime_fact.h
@@ -0,0 +1,20 @@
+// Contiene y define lo que compone la clase
+
+#ifndef PRIME_FACT_H
+#define PRIME_FACT_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+/* Métodos */
+
+// Lee una línea de texto
+uint64_t lecturaDatos(char* linea);
+
+// Calcula los factores primos y los agrega a una lista
+uint64_t calculoFactores(uint64_t numero, uint64_t* arregloFactores, uint64_t* tamanioArreglo);
+
+// Imprime una lista con los factores primos de un número dado
+void impresionDatos(uint64_t numero, uint64_t* arregloFactores, uint64_t* tamanioArreglo);
+
+#endif // PRIME_FACT_H
