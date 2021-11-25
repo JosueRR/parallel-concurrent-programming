ada9f92ecf9af83cbfa81f52140488055e0d6ec9
JosueRR <josue-retana@hotmail.com>
2021-11-24 19:25:57 -0600

    Agrega subrutina print_result

--- Tareas/Tarea04/primefact_omp_mpi/src/distribute.c
+++ Tareas/Tarea04/primefact_omp_mpi/src/distribute.c
@@ -9,6 +9,26 @@
 #include <mpi.h>
 
 
+void print_result(int process_number, int process_count,
+EstructuraArreglo arreglo) {
+    const int previous_process = (process_count + process_number - 1)
+        % process_count;
+    const int next_process = (process_number + 1) % process_count;
+    bool can_print = true;
+    if (process_number > 0) {
+        // receive(&can_print, 1, previous_process)
+        if (MPI_Recv(&can_print, /*count*/ 1, MPI_C_BOOL, previous_process
+        , /*tag*/ 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE) != MPI_SUCCESS ) {
+        printf("could not receive message\n");
+        }
+    }
+    printResult(arreglo);
+    // send(can_print, 1, next_process);
+    if (MPI_Send(&can_print, /*count*/ 1, MPI_C_BOOL, next_process, /*tag*/ 1
+        , MPI_COMM_WORLD) != MPI_SUCCESS) {
+        printf("could not send message\n");
+    }
+}
 
 int calculate_start(int rank, int end, int workers, int begin) {
   const int range = end - begin;
