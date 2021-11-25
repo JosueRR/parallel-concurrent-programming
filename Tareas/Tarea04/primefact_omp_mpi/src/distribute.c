8ad34ba5dc88b59ff8f849504d87fb792fddaff8
JosueRR <josue-retana@hotmail.com>
2021-11-24 19:25:23 -0600

    Agrega subrutina calculate_start

--- Tareas/Tarea04/primefact_omp_mpi/src/distribute.c
+++ Tareas/Tarea04/primefact_omp_mpi/src/distribute.c
@@ -10,6 +10,15 @@
 
 
 
+int calculate_start(int rank, int end, int workers, int begin) {
+  const int range = end - begin;
+  int min = range % workers;
+  if (rank < min) {
+      min = rank;
+  }
+  return begin + rank * (range / workers) + min;
+}
+
 int calculate_finish(int rank, int end, int workers, int begin) {
   return calculate_start(rank + 1, end, workers, begin);
 }
